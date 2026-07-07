a29ea91952e9668ecb95620ed2fa1c9a313ea88e
Adrien Monteiro <adrien200102@gmail.com>
2026-07-07 10:20:34 +0200

    feat(core): add configuration engine

--- analytics_framework/__init__.py
+++ analytics_framework/__init__.py
@@ -1,7 +1,9 @@
+from analytics_framework.core.config import Config
 from analytics_framework.core.pipeline import Pipeline
 from analytics_framework.core.pipeline_step import PipelineStep
 
 __all__ = [
+    "Config",
     "Pipeline",
     "PipelineStep",
 ]
